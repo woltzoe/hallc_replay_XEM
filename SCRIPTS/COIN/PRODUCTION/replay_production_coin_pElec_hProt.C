c80b300fce1c318db56f1039b9695da651515fee
Abhyuday Sharda <ashard@ifarm2402.jlab.org>
2024-11-04 15:24:42 -0500

    Fixed a bug in the coincidence replay scripts

--- SCRIPTS/COIN/PRODUCTION/replay_production_coin_pElec_hProt.C
+++ SCRIPTS/COIN/PRODUCTION/replay_production_coin_pElec_hProt.C
@@ -11,7 +11,7 @@ void replay_production_coin_pElec_hProt (Int_t RunNumber = 0, Int_t MaxEvent = 0
     cin >> MaxEvent;
     if(MaxEvent == 0) {
       cerr << "...Invalid entry\n";
-      exit;
+      exit(1);
     }
   }
 
